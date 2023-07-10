9802109b6cd4d3588a75ed6921bfa94d9cefb57c
lillarado <105851345+lillarado@users.noreply.github.com>
2023-07-10 13:19:09 +0200

    fontawesome

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 import 'jquery';
 import * as Popper from '@popperjs/core';
 import 'bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHeart);
